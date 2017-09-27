38b9bec1ea1c5995dc6727a0049ddb3ab5b22eb7
Joni <jonas.sarbach@lernende.bfo-vs.ch>
2017-09-27 16:07:17 +0200

    Grundgerüst
    Grundgerüst erstellt

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
-	echo "Hello World";
-	echo "Test 2";
-?>
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <title>Startseite</title>
+	<link href="style.css" rel="stylesheet"> 
+  </head>
+  <body>
+   <div id="header"></div>
+   <?php include("nav.php"); ?>
+   <div id="content"></div><!--Ende Content!-->
+   <div id="footer"></div><!--Ende Footer!-->
+ </body>
+</html>
