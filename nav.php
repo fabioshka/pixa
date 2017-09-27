2908e317373d003acd7d587b9d03bbadcf843d5d
fabioshka <31928583+fabioshka@users.noreply.github.com>
2017-09-27 16:16:54 +0200

    nav

--- nav.php
+++ nav.php
@@ -1,4 +1,7 @@
-<?php
-
-echo "wallah";
-?>
+<div id="nav">
+<ul>
+	<a href="index.php"><li>HOME</li></a>
+	<a href="#"><li>ENTDECKEN</li></a>
+	<a href="#"><li>MEIN PROFIL</li></a>
+</ul>
+</div><!--Ende nav-->
