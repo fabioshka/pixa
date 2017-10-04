3998b42e3bd9a450ebc21f8669f77ef3b20e86bb
Jonas <32133108+jonsar@users.noreply.github.com>
2017-10-04 16:35:34 +0200

    Anmeldung
    Basics fürs Anmelde-Formular

--- nav.php
+++ nav.php
@@ -2,6 +2,7 @@
 <ul>
 	<a href="index.php"><li>HOME</li></a>
 	<a href="#"><li>ENTDECKEN</li></a>
-	<a href="#"><li>MEIN PROFIL</li></a>
+	<a href="profil.php"><li>MEIN PROFIL</li></a>
+	
 </ul>
 </div><!--Ende nav-->
--- profil.php
+++ profil.php
@@ -6,9 +6,21 @@
 	<link href="style.css" rel="stylesheet"> 
   </head>
   <body>
-   <div id="header"></div>
-   <?php include("nav.php"); ?>
-   <div id="content"></div><!--Ende Content!-->
-   <div id="footer"></div><!--Ende Footer!-->
+	<div id="header"></div>
+	<?php include("nav.php"); ?>
+	<div id="bg"></div>
+	<div id="content">
+	<h1> Anmelden </h1>
+	<form id="form">
+  Username:<br>
+  <input type="text" name="firstname"><br>
+  Passwort:<br>
+  <input type="text" name="lastname"><br>
+    <input type="submit" value="Submit" name="button">
+</form>
+	
+	
+	</div><!--Ende Content!-->
+	<div id="footer"></div><!--Ende Footer!-->
  </body><!-- Ende Body!-->
 </html>
