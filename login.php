c13826341c64f5b745376176f0fffd975285d280
fabioshka <31928583+fabioshka@users.noreply.github.com>
2017-11-08 16:55:22 +0100

    DIV 2

--- login.php
+++ login.php
@@ -30,7 +30,7 @@ if(isset($errorMessage)) {
 	<input type="email" size="40" maxlength="250" name="email"><br><br>
 	 
 	Dein Passwort:<br>
-	<input type="password" size="40"  maxlength="250" name="passwort"><br>
+	<input type="password" size="40"  maxlength="250" name="passwort"><br><br>
 	 
 	<input type="submit" value="Abschicken">
 	</form>
