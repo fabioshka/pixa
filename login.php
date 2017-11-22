83e06b6ae54e69ad49235a928d89c0ca8c7ae22a
jonsar <32133108+jonsar@users.noreply.github.com>
2017-11-22 15:04:12 +0100

    Login
    Login

--- login.php
+++ login.php
@@ -1,4 +1,37 @@
-<?php 
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <title>Startseite</title>
+	<link href="style.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700" rel="stylesheet">
+	<style>
+				#bg {
+			/* The image used */
+			background-image: url("img/country.jpg");
+
+			/* Full height */
+			height: 100%; 
+
+			/* Center and scale the image nicely */
+			background-position: center;
+			background-repeat: no-repeat;
+			background-size: cover;
+		}
+	</style>
+  </head>
+  <body>
+	<?php include("nav.php"); ?>
+	<div id="header1">
+		<p id="top">PIXA</p>
+		<p id="bottom">Share your pictures!</p>
+	</div>
+   <div id="bg"></div><!--Ende bg-->
+   <div id="clear"></div><!--Ende clear-->
+   
+   <div id="content">
+   <h1>Registrierung</h1>
+	<?php 
 session_start();
 $pdo = new PDO('mysql:host=localhost;dbname=pixa', 'root', '');
  
@@ -24,14 +57,20 @@ if(isset($errorMessage)) {
  echo $errorMessage;
 }
 ?>
- <div id="login">
-	<form action="?login=1" method="post">
-	E-Mail:<br>
-	<input type="email" size="40" maxlength="250" name="email"><br><br>
+</body>
+		<div id="login">
+			<form action="?login=1" method="post">
+				E-Mail:<br>
+				<input type="email" size="40" maxlength="250" name="email"><br><br>
 	 
-	Dein Passwort:<br>
-	<input type="password" size="40"  maxlength="250" name="passwort"><br><br>
+				Dein Passwort:<br>
+				<input type="password" size="40"  maxlength="250" name="passwort"><br><br>
 	 
-	<input type="submit" value="Abschicken">
-	</form>
-</div><!--Ende login-->
+				<input type="submit" value="Abschicken">
+			</form>
+		</div><!--Ende login-->
+   </div><!--Ende Content!-->
+   <div id="footer"></div><!--Ende Footer!-->
+ </body><!-- Ende Body!-->
+</html>
+
