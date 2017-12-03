4b16555079eef930ba3b0ea2467d948aed002c3a
fabioshka <31928583+fabioshka@users.noreply.github.com>
2017-12-03 15:21:00 +0100

    verb

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 			background-image: url("img/country.jpg");
 
 			/* Full height */
-			height: 100%; 
+			height: 100%;
 
 			/* Center and scale the image nicely */
 			background-position: center;
@@ -28,30 +28,33 @@
 	</div>
    <div id="bg"></div><!--Ende bg-->
    <div id="clear"></div><!--Ende clear-->
-   
+
    <div id="content">
-	
+
 	<h1>Was ist PIXA?</h1>
-	
+
 	<img src="img/macbook.jpg" title="MacBook" style="width:50%; float:left; margin-right:1.5em;"/>
-	
+
 	<h2>Lorem Ipsum</h2>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-	Vel eros donec ac odio tempor orci dapibus. Mattis rhoncus urna neque viverra. Ac felis donec et odio. Morbi tristique senectus 
-	et netus et malesuada. Eros donec ac odio tempor orci dapibus. Sit amet venenatis urna cursus eget. Dignissim enim sit amet 
-	venenatis urna cursus eget nunc. Pellentesque sit amet porttitor eget dolor morbi non. Tortor pretium viverra suspendisse potenti. </p><br><br><br>
-	
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+	Vel eros donec ac odio tempor orci dapibus. Mattis rhoncus urna neque viverra. Ac felis donec et odio. Morbi tristique senectus
+	et netus et malesuada. Eros donec ac odio tempor orci dapibus. Sit amet venenatis urna cursus eget. Dignissim enim sit amet
+	venenatis urna cursus eget nunc. Pellentesque sit amet porttitor eget dolor morbi non. Tortor pretium viverra suspendisse potenti. </p>
+
+    <div id="clear"></div>
+    <div id="absatz"></div>
+
 	<img src="img/foto.jpg" title="Foto" style="width:50%; float:right; margin-left:1.5em;"/>
-	
+
 	<h2>Dolor sit amet</h2>
-	<p>A scelerisque purus semper eget duis at tellus. Suspendisse sed nisi lacus sed. Varius duis at consectetur lorem donec. Non consectetur a erat nam 
-	at lectus urna duis convallis. Magna fermentum iaculis eu non diam phasellus. Accumsan lacus vel facilisis volutpat est velit egestas. Vitae et leo 
-	duis ut diam quam nulla porttitor massa. Dolor purus non enim praesent elementum. Nisl nisi scelerisque eu ultrices vitae. Tellus pellentesque eu tincidunt 
+	<p>A scelerisque purus semper eget duis at tellus. Suspendisse sed nisi lacus sed. Varius duis at consectetur lorem donec. Non consectetur a erat nam
+	at lectus urna duis convallis. Magna fermentum iaculis eu non diam phasellus. Accumsan lacus vel facilisis volutpat est velit egestas. Vitae et leo
+	duis ut diam quam nulla porttitor massa. Dolor purus non enim praesent elementum. Nisl nisi scelerisque eu ultrices vitae. Tellus pellentesque eu tincidunt
 	tortor aliquam nulla. Morbi tristique senectus et netus et.</p>
-	
+
    </div><!--Ende Content!-->
    <div id="footer">
-	
+
    </div><!--Ende Footer!-->
  </body><!-- Ende Body!-->
 </html>
