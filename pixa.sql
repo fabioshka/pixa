0691dee1eefb960b4441579f6159fdc299e79535
fabioshka <31928583+fabioshka@users.noreply.github.com>
2017-12-05 19:32:07 +0100

    sql

--- pixa.sql
+++ pixa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.3
+-- version 4.7.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Erstellungszeit: 04. Dez 2017 um 22:03
+-- Erstellungszeit: 05. Dez 2017 um 19:31
 -- Server-Version: 5.6.35
--- PHP-Version: 7.1.8
+-- PHP-Version: 7.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,15 +26,20 @@ CREATE TABLE `bilder` (
   `name` varchar(255) NOT NULL,
   `beschrieb` varchar(500) NOT NULL,
   `kategorie` varchar(255) NOT NULL,
-  `link` varchar(255) NOT NULL
+  `link` varchar(255) NOT NULL,
+  `datum` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Daten für Tabelle `bilder`
 --
 
-INSERT INTO `bilder` (`bilderid`, `fotograf_id`, `name`, `beschrieb`, `kategorie`, `link`) VALUES
-(7, 1, 'Fotografin', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et e', 'portrait', 'img/foto.jpg');
+INSERT INTO `bilder` (`bilderid`, `fotograf_id`, `name`, `beschrieb`, `kategorie`, `link`, `datum`) VALUES
+(7, 1, 'Fotografin', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et e', 'Portrait', 'img/foto.jpg', '2017-12-03 23:00:00'),
+(8, 1, 'Landschaft', 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. ', 'Natur', 'img/country_1.jpg', '2017-12-04 23:00:00'),
+(9, 1, 'Bild 5', 'Lorem Ipsum dolor', 'Natur', 'img/Berg.jpg', '2017-12-05 12:32:13'),
+(10, 1, 'Landschaft 2', 'sdlfhsdk skdjfhskdjfh sdkfhsdkjfh skdfhs fkjhsdf ', 'Natur', 'img/IMG_0017.jpg', '2017-12-05 14:57:51'),
+(11, 1, 'Test 100', 'dkjf skdjfhhsd sdkf skdjf dweguirwt. cvxc saie xbvhx', 'Natur', 'img/IMG_0935.jpg', '2017-12-05 15:43:12');
 
 -- --------------------------------------------------------
 
@@ -85,7 +90,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT für Tabelle `bilder`
 --
 ALTER TABLE `bilder`
-  MODIFY `bilderid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `bilderid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT für Tabelle `users`
 --
